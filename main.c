2737cf552ef3f408e647ef9ff2e64906f5078470
Xiaodong Li <bryne_lxd@sina.com>
2019-10-08 14:24:33 +0800

    updated .gitignore for cmake and vscode

---
+++ main.c
@@ -0,0 +1,33 @@
+//
+// Created by LI XIAO DONG on 2019/10/8.
+//
+
+#include <stdio.h>
+
+float Q_rsqrt( float number )
+{
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    y  = number;
+    i  = * ( long * ) &y;						// evil floating point bit level hacking
+    i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+    y  = * ( float * ) &i;
+    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+#ifndef Q3_VM
+#ifdef __linux__
+    assert( !isnan(y) ); // bk010122 - FPE?
+#endif
+#endif
+    return y;
+}
+
+void main(){
+    int i = 50;
+    float sofi = Q_rsqrt(i);
+    printf("the square of i is: %d", sofi);
+}
